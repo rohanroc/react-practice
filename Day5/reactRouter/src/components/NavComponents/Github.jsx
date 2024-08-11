87fd01f3847cbc09a0e9b553adadc4b51f742539
Arijit <saha65092@gmail.com>
2024-08-11 21:21:03 +0530

    added github api

--- Day5/reactRouter/src/components/NavComponents/Github.jsx
+++ Day5/reactRouter/src/components/NavComponents/Github.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://api.github.com/users/rohanroc').then(function (responce) {
+            return responce.json();
+        }).then(function (data) {
+            console.log(data)
+            setData(data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }, [])
     return (
-        <div>
-            GitHub
+        <div className='bg-gray-400 text-3xl text-center py-2.5'>
+            GitHub followers: {data.followers}
+            <img src={data.avatar_url} alt="" className='w-1/4 rounded-full m-auto my-5' />
+            <p>Name: {data.name}</p>
         </div>
     )
 }
